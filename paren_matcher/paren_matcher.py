681f468be2d313986a15385cb4739b6742e8d307
johnngugi <johnngugi777@gmail.com>
2016-08-05 19:09:37 +0300

    Add solution for paren_matcher

--- paren_matcher/paren_matcher.py
+++ paren_matcher/paren_matcher.py
@@ -6,3 +6,22 @@
 #   '((()'
 #   '())('
 
+
+def paren_matcher(parens):
+    opening = 0
+    closing = 0
+
+    for x in range(len(parens)):
+        if parens[x] == '(':
+            opening += 1
+        elif parens[x] == ')':
+            closing += 1
+
+    if opening == closing:
+        print "Balanced"
+    else:
+        print "Not Balanced"
+
+paren_matcher("(((())))")
+
+
