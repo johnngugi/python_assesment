fb050961d09e3123ffbfd0e917f156b312940205
valdadyne <rodgers.gitau@gmail.com>
2016-08-05 15:52:40 +0300

    Added Solution

--- question4.py
+++ question4.py
@@ -3,14 +3,22 @@ import random
 
 
 def main(size=10):
-    lcase = string.ascii_lowercase[:26]
-    ucase = string.ascii_uppercase[:26]
-    numbers = string.digits
-    symbols = random.choice('%&#$@')
-    pwd = lcase + ucase + numbers + symbols
+    user = raw_input("Do you want to your password to be: (A) Strong (B) Weak [A/B]?\n")
 
-    return ''.join(random.choice(pwd) for i in range(size))
+    if user == "A"or user == "a":
+        lcase = string.ascii_lowercase[:26]
+        ucase = string.ascii_uppercase[:26]
+        numbers = string.digits
+        symbols = random.choice('%&#$@')
+        pwd = lcase + ucase + numbers + symbols
+        return ''.join(random.choice(pwd) for p in range(size))
+
+    elif user == "B"or user == "b":
+        pwd = ('password', '2016', 'user')
+        return random.choice(pwd)
+    else:
+        return 'Wrong Entry, Retry'
+
 
 if __name__ == "__main__":
-    main()
-print main()
+    print main()
